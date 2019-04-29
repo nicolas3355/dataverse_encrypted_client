d9d493db63d7ae5246b6eda8cf197971a787a738
nicolas3355 <nicolaselhaddad.nh@gmail.com>
2019-04-29 18:16:55 -0400

    added the function to wrap and unwrap the symmetric key that encrypts the data

--- enc_file.py
+++ enc_file.py
@@ -1,6 +1,7 @@
 # Used for encrypting the files to be uploaded to dataverse
 from miscreant.aes.siv import SIV
 import os
+import hashlib
 
 
 def enc_file(filepath):
@@ -33,9 +34,38 @@ def wrap_key_org(public_key_org, data_key):
 
 
 def wrap_key_owner(passphrase, data_key):
-    # salt = ejre
-    # owner_key = hmac(passphrase, salt)
+    iterations = 10000
+    dklen = 32
+    salt = os.urandom(16)
+    # owner_key = hmac(passphrase, so
+    # hashlib.pbkdf2_hmac: (hash_name, password, salt, iterations, dklen=None)
+    derived_key = hashlib.pbkdf2_hmac("sha512", passphrase, salt, iterations,
+                                      dklen)
+
+    # argon2 implementation
+    # h = argon2.PasswordHasher()
+    # https://argon2-cffi.readthedocs.io/en/stable/argon2.html
+    # better use argon2i for our use case
+    # u'$argon2id$v=19$m=102400,t=2,p=8$fUQzqlLHaEi+iTknR8cfpA$4Qf2rZ/SfBrTvzdEkDJUsQ'
+
     # ciphertext = encrypt(data_key, owner_key)
-    # ciphertext = salt + ciphertext
-    ciphertext = "123"
+    siv = SIV(derived_key)
+    ciphertext = siv.seal(data_key)
+
+    # append the salt that was used to generate the derived key, with the
+    # ciphertext
+    ciphertext = salt + ciphertext
     return ciphertext
+
+
+def unwrap_key_owner(passphrase, wrapped_key_ciphertext):
+    iterations = 10000
+    dklen = 32
+    salt = wrapped_key_ciphertext[:16]
+
+    derived_key = hashlib.pbkdf2_hmac("sha512", passphrase, salt, iterations,
+                                      dklen)
+
+    siv = SIV(derived_key)
+    data_key = siv.open(wrapped_key_ciphertext[16:])
+    return data_key
